04b7214b157231bcf2237f2ea77295abc519ddb3
muthuknm <85172977+muthuknm@users.noreply.github.com>
2021-06-30 18:00:48 +0530

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let x:Double = 1
+
+let c = true
+if (c) {
+    print ("true")
+}
+
+let array = [Int]()
+
+
+
+
+
+
+let noValue:Int? = nil
+let unwrappedValue = noValue!
+
+
+
+
+r = min(0, max(255, r))
